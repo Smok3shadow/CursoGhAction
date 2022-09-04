f0a1a1dd70318450e1b3c5dc7c7768a473d8d591
patricio albornoz <patricio.albornoz05@gmail.com>
2022-09-03 22:35:44 -0400

    Se agrega archivo deply.yml

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://smok3shadow.github.io/',
+  base: 'https://smok3shadow.github.io/CursoGhAction/',
   plugins: [react()]
 })
